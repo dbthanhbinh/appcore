74c1bf7cd92fcfe77c16776c74e1854327bc4614
dbthanhbinh <dbthanhbinh@gmail.com>
2019-10-23 09:42:22 +0700

    admin-upload-file

---
+++ FileService/Commons.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileService
+{
+    public class Commons
+    {
+        private static readonly IDictionary<string, string> ImageMimeDictionary = new Dictionary<string, string>
+        {
+            { ".bmp", "image/bmp" },
+            { ".dib", "image/bmp" },
+            { ".gif", "image/gif" },
+            { ".svg", "image/svg+xml" },
+            { ".jpe", "image/jpeg" },
+            { ".jpeg", "image/jpeg" },
+            { ".jpg", "image/jpeg" },
+            { ".png", "image/png" },
+            { ".pnz", "image/png" }
+        };
+
+        public bool AllowMimeTypesFile()
+        {
+            bool IsAllow = true;
+
+            return IsAllow;
+        }
+
+        public string DirectoryUpload()
+        {
+            return "Uploads";
+        }
+
+        // Create Directory if is not exists
+        public void CreateDirectoryIfNotExists()
+        {
+            string FullDirectoryPath = this.GetFullDirectoryPathUpload();
+            bool Exists = Directory.Exists(FullDirectoryPath);
+            if (!Exists)
+                Directory.CreateDirectory(FullDirectoryPath);
+        }
+
+        public string GetCurrentDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        public string GetFullDirectoryPathUpload()
+        {
+            return this.GetCurrentDirectory() + "\\" + this.DirectoryUpload();
+        }
+
+        public string GetFullPathUploadFile(string FileName)
+        {
+            return this.GetCurrentDirectory() + "\\" + this.DirectoryUpload() + "\\" + FileName;
+        }
+
+        // Create new fileName if is Exists
+        public string CreateFileName(string FileName)
+        {
+            if (!string.IsNullOrEmpty(FileName) && File.Exists(this.GetFullPathUploadFile(FileName)))
+            {
+                string OldFileName = Path.GetFileNameWithoutExtension(FileName);
+                string Extension = Path.GetExtension(FileName);
+                string NewFileName = OldFileName + "_" + ;
+                FileName = NewFileName + Extension;
+            }
+            return FileName;
+        }
+    }
+}
---
+++ FileService/FileLogic.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileService
+{
+    public class FileLogic
+    {
+        public async Task UploadFile(IFormFile file)
+        {
+            bool isCopied = false;
+            try
+            {
+                Commons common = new Commons();
+                string Uploaded = common.DirectoryUpload();
+                // Create Uploads folder if not exists
+                string CurrentDirectory = common.GetCurrentDirectory();
+                common.CreateDirectoryIfNotExists();
+
+                //1 check if the file length is greater than 0 bytes 
+                if (file.Length > 0)
+                {
+                    string fileName = common.CreateFileName(file.FileName);
+
+                    //3 check the file extension as png
+                    if (common.AllowMimeTypesFile())
+                    {
+                        //4 set the path where file will be copied
+                        string filePath = Path.GetFullPath(
+                            Path.Combine(CurrentDirectory, Uploaded));
+                        //5 copy the file to the path
+                        using (var fileStream = new FileStream(
+                            Path.Combine(filePath, fileName),
+                                           FileMode.Create))
+                        {
+                            await file.CopyToAsync(fileStream);
+                            isCopied = true;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("File must be either .png or .JPG");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
