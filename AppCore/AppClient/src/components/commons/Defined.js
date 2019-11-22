5a272c809a36aed17b733bbfe97fa5c6a19fd8a3
dbthanhbinh <dbthanhbinh@gmail.com>
2019-11-22 21:29:58 +0700

    Update package, Delete AppClient

--- AppCore/AppClient/src/components/commons/Defined.js
+++
@@ -1,9 +0,0 @@
-export const PostDefined = {
-    NAME: 'name',
-    CONTENT: 'content',
-    CATEGORYID: 'categoryid',
-    FILE: 'file',
-    SEOTITLE: 'seo_title',
-    SEOKEYS: 'seo_keys',
-    SEODESCRIPTION: 'seo_description'
-}
